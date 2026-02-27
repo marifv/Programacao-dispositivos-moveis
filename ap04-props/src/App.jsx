a35f54ccbfa26cbabb1a164220ed5f13bf2b288c
Marianafv <148827896+marifv@users.noreply.github.com>
2026-02-27 18:50:17 -0300

    feat: adiciona responsividade

---
+++ ap04-props/src/App.jsx
@@ -0,0 +1,26 @@
+const App = () => (
+    <div className="container border mt-2">
+        <div className="row">
+            <div className="col-12">
+                <i className="fa fa-solid fa-cat"></i>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-sm-12 col-lg-6 col-xxl-3">
+                <div className="py-2">Pedido 1</div>
+            </div>
+            <div className="col-sm-12 col-lg-6 col-xxl-3">
+                <div className="py-2">Pedido 2</div>
+            </div>
+            <div className="col-sm-12 col-lg-6 col-xxl-3">
+                <div className="py-2">Pedido 3</div>
+            </div>
+            <div className="col-sm-12 col-lg-6 col-xxl-3">
+                <div className="py-2">Pedido 4</div>
+            </div>
+        </div>
+    </div>
+)
+
+export default App
