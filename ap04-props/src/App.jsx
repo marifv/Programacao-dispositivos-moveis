7a612a1bdae45e0c6675bc47eb70409c350e863c
Marianafv <148827896+marifv@users.noreply.github.com>
2026-02-27 19:00:23 -0300

    refactor: melhora a organização do App

--- ap04-props/src/App.jsx
+++ ap04-props/src/App.jsx
@@ -1,26 +1,31 @@
-const App = () => (
+    const App = () => (
     <div className="container border mt-2">
         <div className="row">
-            <div className="col-12">
-                <i className="fa fa-solid fa-cat"></i>
-            </div>
+        <div className="col-12">
+            {/* .i.fa.fa-hippo */}
+            <div className="fa fa-cat fa-2x p-3"></div>
+        </div>
         </div>
 
         <div className="row">
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="py-2">Pedido 1</div>
-            </div>
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="py-2">Pedido 2</div>
-            </div>
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="py-2">Pedido 3</div>
-            </div>
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="py-2">Pedido 4</div>
-            </div>
+        {/* .col-sm-12.col-lg-6.col-xxl-3*4 */}
+        <div className="col-sm-12 col-lg-6 col-xxl-3">
+            <div className="py-2 border">Pedido 1</div>
+        </div>
+
+        <div className="col-sm-12 col-lg-6 col-xxl-3">
+            <div className="py-2 border">Pedido 2</div>
+        </div>
+
+        <div className="col-sm-12 col-lg-6 col-xxl-3">
+            <div className="py-2 border">Pedido 3</div>
+        </div>
+
+        <div className="col-sm-12 col-lg-6 col-xxl-3">
+            <div className="py-2 border">Pedido 4</div>
+        </div>
         </div>
     </div>
-)
+    );
 
-export default App
+    export default App;
